79c771070875bb8dad52451dcb4ea0c7f9709a0d
Wei-Tsung Su <ellington.su@gmail.com>
2025-11-21 15:10:55 +0800

    [update] add default route to server in blue_lab_firewall

--- blue_lab_firewall/server/start.sh
+++ blue_lab_firewall/server/start.sh
@@ -6,5 +6,10 @@
 # start ftp
 /usr/sbin/vsftpd /etc/vsftpd.conf &
 
+# 刪除預設路由（若沒有也不會失敗）
+ip route del default 2>/dev/null || true;
+# 新增經由 firewall 的預設路由（若已存在也忽略錯誤）
+ip route add default via 172.21.0.254;
+
 # start nginx in foreground
 nginx -g "daemon off;"
